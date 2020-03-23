136bca009877ac654cea4dd225f08c1cfa8738d6
Stheven Cabral <stheven.cabral@gmail.com>
2020-03-22 21:05:49 -0700

    03222020  2105

--- js/app.js
+++ js/app.js
@@ -3,9 +3,18 @@ const introEnterButton = document.getElementById('intro-overlay-enter');
 const introOverlay = document.getElementById('intro-overlay');
 
 function overlayOff() {
-    console.log('hello');
     introOverlay.style.visibility = "hidden";
     introOverlay.style.opacity = 0.1;
   }
 
 introEnterButton.addEventListener('click', overlayOff);
+
+/*Code to turn on overlay when 'Home' is clicked on the navigation bar*/
+const homeNavButton = document.getElementById('home-button');
+
+function overlayOn() {
+    introOverlay.style.visibility = "visible";
+    introOverlay.style.opacity = 1;
+  }
+
+homeNavButton.addEventListener('click', overlayOn);
