1ea95e6b5f6a53bdb26cc6c5972f173837a1a3f2
Stheven Cabral <stheven.cabral@gmail.com>
2020-03-28 09:16:46 -0700

    Completed interface.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 /*Code to close overlay when 'ENTER' is clicked*/
 const introEnterButton = document.getElementById('intro-overlay-enter');
 const introOverlay = document.getElementById('intro-overlay');
+const navBar = document.getElementById('nav-bar');
 
 function overlayOff() {
-    introOverlay.style.visibility = "hidden";
-    introOverlay.style.opacity = 0.1;
-  }
+  introOverlay.style.opacity = 0.1;
+  introOverlay.style.visibility = "hidden";
+  navBar.style.opacity = 1;
+  navBar.style.visibility = "visible";
+}
 
 introEnterButton.addEventListener('click', overlayOff);
 
@@ -13,8 +16,10 @@ introEnterButton.addEventListener('click', overlayOff);
 const homeNavButton = document.getElementById('home-button');
 
 function overlayOn() {
-    introOverlay.style.visibility = "visible";
-    introOverlay.style.opacity = 1;
-  }
+  introOverlay.style.opacity = 1;
+  introOverlay.style.visibility = "visible";
+  navBar.style.opacity = 0.1;
+  navBar.style.visibility = "hidden";
+}
 
 homeNavButton.addEventListener('click', overlayOn);
