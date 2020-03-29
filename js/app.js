0a353a81212384deac609eea054525dad6ce878e
Stheven Cabral <stheven.cabral@gmail.com>
2020-03-29 16:19:07 -0700

    032920290  1619

--- js/app.js
+++ js/app.js
@@ -24,8 +24,9 @@ function overlayOn() {
 
 homeNavButton.addEventListener('click', overlayOn);
 
-/*jQuery Code for smooth scrolling when navigation links are clicked*/
+/*jQuery Code for portfolio effects*/ 
 $('document').ready(function() {
+  /*Code for smooth scrolling when navigation links are clicked*/
   $('.nav-item-page').click(function(e) {
     let innerHref = $(this).attr('href');
     
@@ -34,5 +35,26 @@ $('document').ready(function() {
     }, 1000);
 
     e.preventDefault();
+  });
+
+  /*Code for fade in effect for skills, project and contact section*/
+  const skillsSection = $('.skills-text');
+  const skillsSectionOffset = skillsSection.offset().top;
+  const contactSection = $('.contact-container');
+  const contactSectionOffset = contactSection.offset().top;
+  console.log(skillsSectionOffset);
+  console.log(contactSectionOffset);
+
+  $(document).on('scroll', function() {
+    let portfolioDocument = $(document);
+    console.log(portfolioDocument.scrollTop());
+
+    if(portfolioDocument.scrollTop() > skillsSectionOffset - 500) {
+      skillsSection.removeClass('skills-text-hidden');
+    }
+
+    if(portfolioDocument.scrollTop() > contactSectionOffset - 985) {
+      contactSection.removeClass('contact-container-hidden');
+    } 
   });
 });
